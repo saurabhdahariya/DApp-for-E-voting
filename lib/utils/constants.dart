9114488158273e1147b29b754e71d20173656738
Saurabh Dahariya <saurabhdahariya81@outlook.com>
2022-08-24 21:22:49 +0530

    init

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+String infura_url =
+    "https://kovan.infura.io/v3/27ec05d6822442a38a7e27a74309ac1c";
+
+String owner_private_key =
+    "a9fe0698877653c930bf786448fad5b28a279e1d30a67f057ee0e5b8166bcb6d";
+
+String voter_private_key =
+    "b17256687b9d97d0ca86728690e488137629e2050bff033b82d89a8b2493f94c";
+
+String contractAddress1 = "0xd9145CCE52D386f254917e481eB44e9943F39138";
